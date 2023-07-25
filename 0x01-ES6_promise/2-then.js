d7c58863c14ed1d459b6211d1e0f329a06201c75
micoliser <micoliser@gmail.com>
2023-07-25 10:58:35 +0100

    Mandatory + Advanced done

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+export default async function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    })
+    .catch(() => Error());
+}
---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((res) => res);
+}
