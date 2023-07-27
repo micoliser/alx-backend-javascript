361542c5544fcb7740ef93121fa30debe274ad79
micoliser <micoliser@gmail.com>
2023-07-27 18:33:16 +0100

    Mandatory + Advanced

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-underscore-dangle */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
+    if (typeof range !== 'string') {
+      throw new Error('Range must be a string');
+    }
+    this._range = range;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  cloneCar() {
+    return new Car();
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,1 @@
+# Classes ES6
