6c29624955a26e8d10d8c5db4b3e260e62809e74
micoliser <micoliser@gmail.com>
2023-07-27 21:53:19 +0100

    Promises fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default async function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
-    .then(() => console.log('Got a response from the API'));
+    .finally(() => console.log('Got a response from the API'));
 }
