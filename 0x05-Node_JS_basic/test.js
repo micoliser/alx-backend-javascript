1a8c6961f7bbabbf6ee33b0d38756506705483fd
micoliser <micoliser@gmail.com>
2023-09-20 08:45:43 +0100

    more fixes

--- 0x05-Node_JS_basic/test.js
+++ 0x05-Node_JS_basic/test.js
@@ -14,21 +14,22 @@ describe('countStudents', () => {
     consoleSpy.restore();
   });
 
-  it('logs to the console the right messages', (done) => {
-    countStudents('./database.csv').then(() => {
-      expect(consoleSpy.calledWith('Number of students: 10')).to.be.true;
-      expect(
-        consoleSpy.calledWith(
-          'Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie'
-        )
-      ).to.be.true;
-      expect(
-        consoleSpy.calledWith(
-          'Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy'
-        )
-      ).to.be.true;
-
-      done();
+  it('throws the correct error message', (done) => {
+    countStudents('./blabl.csv').catch((error) => {
+      expect(error).to.equal(new Error('Error: Cannot load the database'));
     });
+    expect(consoleSpy.calledWith('Number of students: 10')).to.not.be.true;
+    expect(
+      consoleSpy.calledWith(
+        'Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie'
+      )
+    ).to.not.be.true;
+    expect(
+      consoleSpy.calledWith(
+        'Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy'
+      )
+    ).to.not.be.true;
+
+    done();
   });
 });
